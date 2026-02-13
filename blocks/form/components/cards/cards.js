38aef47ee28f90f007261cc18c3d994fdf381827
ezhiloli <ezhiloli07@gmail.com>
2026-02-13 19:13:12 +0530

    updates

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -1,12 +1,52 @@
 import { createOptimizedPicture } from '../../../../scripts/aem.js';
-export default function decorate(element, fieldJson, container, formId) {
-  element.classList.add('card');
-  element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
+import { subscribe } from '../../rules/index.js';
+
+function createCard(element, enums) {
+  element.querySelectorAll('.radio-wrapper').forEach((radioWrapper, index) => {
+    if (enums[index]?.name) {
+      let label = radioWrapper.querySelector('label');
+
+      if (!label) {
+        label = document.createElement('label');
+        radioWrapper.appendChild(label);
+      }
+
+      label.textContent = enums[index]?.name;
+    }
+
+    // Attach index to input element for later reference
+    radioWrapper.querySelector('input').dataset.index = index;
+
     const image = createOptimizedPicture(
-      'https://main--adobecustom--earivoli.aem.page/icons/cc.svg',
+      enums[index]?.image || 'https://main--adobecustom--earivoli.aem.page/icons/cc.svg',
       'card-image'
     );
+
     radioWrapper.appendChild(image);
   });
+}
+
+export default function decorate(element, fieldJson, container, formId) {
+  element.classList.add('card');
+  createCard(element, fieldJson.enum);
+
+  subscribe(element, formId, (fieldDiv, fieldModel) => {
+    fieldModel.subscribe((e) => {
+      const { payload } = e;
+
+      payload?.changes?.forEach((change) => {
+        if (change?.propertyName === 'enum') {
+          createCard(element, change.currentValue);
+        }
+      });
+    });
+
+    element.addEventListener('change', (e) => {
+      e.stopPropagation();
+      const value = fieldModel.enum?.[parseInt(e.target.dataset.index, 10)];
+      fieldModel.value = value.name;
+    });
+  });
+
   return element;
 }
